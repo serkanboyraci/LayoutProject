e365305ce26b7a5bc84eacfbab0ad2dd41d91b76
serkanboyraci <serkanboyraci@gmail.com>
2023-09-07 10:32:39 +0200

    updated autolayout

--- LayoutProject/ViewController.swift
+++ LayoutProject/ViewController.swift
@@ -10,27 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var myLabel = UILabel() //label eklemeye yarar.
     var yenidegisken = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var yenidegisken = 3
+        yenidegisken = 3
         print(yenidegisken) //bu 3 ü bastırırken
         print(self.yenidegisken) // bu 4 ü bastırır.
         
         
-        let width = view.frame.size.width //görüntünün çerçevesinin genişliğine eşit bir sabit tanımladık.
-        let height = view.frame.size.height
+        let widthFrame = view.frame.size.width //görüntünün çerçevesinin genişliğine eşit bir sabit tanımladık.
+        let heightFrame = view.frame.size.height //görüntünün çerçevesinin yuksekligine eşit bir sabit tanımladık.
         
         //label
-        /*let myLabel = UILabel() //label eklemeye yarar.
+        
         myLabel.text = "BENIM LABEL" //label ismii verir.
         myLabel.textAlignment = .center // put it on the center.
-        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8/2, //tam ortayı ayarlamak için görüntü çerçevesinin ortasını yarısı alarak ama aynı zamanda labelin da yarısını çıkararak tam ortaya oturturuz.
-                               y: height*0.5 - 30,
-                               width: width*0.8, //labelin genişliğini çerçeveinin genişliğinin 0.8 katına ayarladık.
+        myLabel.frame = CGRect(x: widthFrame * 0.5 - widthFrame * 0.8/2, //tam ortayı ayarlamak için görüntü çerçevesinin ortasını yarısı alarak ama aynı zamanda labelin da yarısını çıkararak tam ortaya oturturuz.
+                               y: heightFrame*0.5 - 30,
+                               width: widthFrame*0.8, //labelin genişliğini çerçeveinin genişliğinin 0.8 katına ayarladık.
                                height: 60)
         //bunun yerine
         //myLabel.frame = CGRect(x: 40, y: 300, width: 100, height: 100) //you must arrange the size and location. // it starts with left upper corner.
@@ -40,24 +41,26 @@ class ViewController: UIViewController {
         let myButton = UIButton()
         myButton.setTitle("BENIM BUTTON", for: .normal) //UIControl.State.normal aynısı demek.
         myButton.setTitleColor(.blue, for: UIControl.State.normal)
-        myButton.frame = CGRect(x: width * 0.5 - width * 0.8/2,
-                                y: height * 0.7,
-                                width: width * 0.8, height: 100)
+        myButton.frame = CGRect(x: widthFrame * 0.5 - widthFrame * 0.8/2,
+                                y: heightFrame * 0.7,
+                                width: widthFrame * 0.8, height: 100)
         view.addSubview(myButton)
         
         //buttonu aktif etme
         myButton.addTarget(self, //self yazarak kendisini hedef gösterilmeli
+                           // VC nin kendisini soyluyoruz, baska bir yerde bisey yapmayacak
                            action: #selector(ViewController.benimfonk), //selector girilmek zorunda.
                            //viewcontroller dışına da objc func yazıp eklenmeli
                            for: UIControl.Event.touchUpInside) //sadece . ya basarak yada
                             //UIControl.Event. yaparak buttonun ne zaman çalıacağını seçebiliriz.
-        */
+        
                            
     
         
     }
     //IBAction func yazsan da olur.
     @objc func benimfonk() {
+        myLabel.text = "User Tapped"
         print("Kullanıcı Tikladi")
     }
 
